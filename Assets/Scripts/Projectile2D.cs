99307efdeb28a9cea067361ae3f73106e79e5f57
Sundayx4 <thirat.chan@bumail.net>
2025-05-01 20:47:36 +0700

    Scripts Raycast

--- Assets/Scripts/Projectile2D.cs
+++ Assets/Scripts/Projectile2D.cs
@@ -1,16 +1,27 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform ShootPoint;
+    [SerializeField] GameObject Target;
+    [SerializeField] Rigidbody2D BulletPrefab;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null) 
+            {
+                Target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit" + hit.collider.name);
+            }
+        }
     }
 }
